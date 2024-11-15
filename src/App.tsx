bcdcda6f04fba0d68b310dbb0b4edcccc2dbf96f
a-maystorov <alkin971@gmail.com>
2024-11-15 17:32:38 +0100

    extend default mantine theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import "./App.css";
 import Home from "./pages/Home";
+import theme from "./themes/mantine-theme";
 
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Home />
     </MantineProvider>
   );
--- src/components/common/AppButton.tsx
+++ src/components/common/AppButton.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@mantine/core";
+import { Button, Title, useMantineTheme } from "@mantine/core";
 import { useState } from "react";
 
 export default function AppButton() {
   const [count, setCount] = useState(0);
+  const theme = useMantineTheme();
 
   return (
-    <Button bg="red.5" onClick={() => setCount((prev) => prev + 1)}>
-      Count: {count}
+    <Button bg={theme.colors.deepBlue[5]} onClick={() => setCount((prev) => prev + 1)}>
+      <Title order={1}>Count: {count}</Title>
     </Button>
   );
 }
