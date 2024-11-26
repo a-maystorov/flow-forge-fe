a884f5dddde8dce43facb12a7366628a5e7b9e70
a-maystorov <alkin971@gmail.com>
2024-11-26 12:32:28 +0100

    chore: adjust formating rules

--- src/components/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle.tsx
@@ -1,11 +1,17 @@
 import { Button, useMantineColorScheme } from '@mantine/core';
 
 export default function ColorSchemeToggle() {
-  const { colorScheme, setColorScheme } = useMantineColorScheme({ keepTransitions: true });
+  const { colorScheme, setColorScheme } = useMantineColorScheme({
+    keepTransitions: true,
+  });
 
   const toggleColorScheme = () => {
     setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
   };
 
-  return <Button onClick={toggleColorScheme}>Toggle {colorScheme === 'dark' ? 'Light' : 'Dark'} Mode</Button>;
+  return (
+    <Button onClick={toggleColorScheme}>
+      Toggle {colorScheme === 'dark' ? 'Light' : 'Dark'} Mode
+    </Button>
+  );
 }
